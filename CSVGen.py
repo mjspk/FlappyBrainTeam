b8dbd684f4fd298de4e8006496c9da0d5c01728f
Derek Walz <derek.walz@ucalgary.ca>
2022-07-30 17:24:14 -0600

    new csv writer

--- CSVGen.py
+++ CSVGen.py
@@ -5,90 +5,109 @@ import EEGDataGen as dataGen
 import msvcrt as keyboard
 import numpy as np
 from Processing import DataReader
+import regex as re
 
-def intro():
-    print("Get Ready to Focus ")
-    print("Please hit the key 'R' when asked to Enter Right and 'L' for Left")
-    index = 5
-    while index > 0:
-        t0 = time.time()
-        print(index)
-        while(True):
-            t1 = time.time()
-            if(t1-t0) > 1:
-                break
-        index -= 1
-        
+class csvWriter:
 
-def inputLoop(dr, fileNameBin, fileNameRaw):
+    def __init__(self, channels, sources) -> None:
+        self.direction = ""
+        self.frequencies = np.array([])
+        self.amplitudes = np.array([])
+        self.bands = np.array([])
+        self.fileNameBin = ""
+        self.fileNameRaw = ""
+        self.dr = DataReader(1000)
+        self.channels = channels
+        self.sources = sources
 
-    endTime = time.time() + randomTime() 
-    direction = randomFlag()
-            #while true, enter the loop, have a time checker that breaks it, but while doing this if you end up 
-            #out of range you need to extend the loop
-    while(time.time() < endTime):
-        readIn = []
-        print("\nENTER " + direction + "\n")
-        keyboard.getch()
-        readIn[0], readIn[1], readIn[2] = dr.get_data()
-        writeFileBin(direction, fileNameBin, readIn)
-        writeFileRaw(direction, fileNameRaw, readIn)
-
+    def intro(self):
+        print("Get Ready to Focus ")
+        print("Please hit the key 'R' when asked to Enter Right and 'L' for Left")
+        index = 5
+        while index > 0:
+            t0 = time.time()
+            print(index)
+            while(True):
+                t1 = time.time()
+                if(t1-t0) > 0.5:
+                    break
+            index -= 1
             
-def writeFileBin(direction, fileName, readIn):
-    size = np.shape(readIn[2])[1]
-    final = ""
-    for i in range(size):
-        final += str(readIn[:,i])[1:-1]
-        final += ", "
-    final += direction + "\n"
-    file = open(fileName, "a")
-    file.write(final)    
 
-def writeFileRaw(direction, fileName, readIn):
-    final  = direction + ", "
-    final += np.vstack([readIn[0], readIn[1]]) + "\n"
-    file = open(fileName, "a")
-    file.write(final)        
-    
-def withinRange(readIn):
-    threshold1 = 13
-    threshold2 = 29
-    input1Valid = readIn[0] > threshold1 and readIn[0] < threshold2
-    input2Valid = readIn[1] > threshold1 and readIn[1] < threshold2
-    if(input1Valid and input2Valid):
-        return True
-    else: 
-        return False
+    def inputLoop(self):
 
-#here for test only
-def randomFlag():
-    newFlag = random.randint(0, 1)
-    if(newFlag == 0):
-        return "Right"
-    else: 
-        return "Left"
+        endTime = time.time() + self.randomTime() 
+        self.direction = self.randomFlag()
+                #while true, enter the loop, have a time checker that breaks it, but while doing this if you end up 
+                #out of range you need to extend the loop
+        while(time.time() < endTime):
+            print("\nENTER " + self.direction + "\n")
+            keyboard.getch()
+            self.frequencies, self.amplitudes, self.bands = self.dr.get_data()
+            self.writeFileBin()
+            self.writeFileRaw()
 
-def randomTime():
-    return random.randint(5, 15)
+                
+    def writeFileBin(self):
+        size = self.channels * self.sources
+        final = ""
+        for i in range(size):
+            final += str(self.bands[i]) # type: ignore
+            final += ", "
+        final += self.direction + "\n"
+        file = open(self.fileNameBin, "a")
+        file.write(final)    
+
+    def writeFileRaw(self):
+        size = self.sources
+        final = str(self.frequencies) + ","
+        for i in range(size):
+            final += str(self.amplitudes[i,:])
+            final += ","
+        final += self.direction + "\n"
+        file = open(self.fileNameRaw, "a")
+        file.write(final)           
+        
+    # def withinRange(readIn):
+    #     threshold1 = 13
+    #     threshold2 = 29
+    #     input1Valid = readIn[0] > threshold1 and readIn[0] < threshold2
+    #     input2Valid = readIn[1] > threshold1 and readIn[1] < threshold2
+    #     if(input1Valid and input2Valid):
+    #         return True
+    #     else: 
+    #         return False
+
+    #here for test only
+    def randomFlag(self):
+        newFlag = random.randint(0, 1)
+        if(newFlag == 0):
+            return "Right"
+        else: 
+            return "Left"
+
+    def randomTime(self):
+        return random.randint(5, 15)
 
 def main():
-
-    
-    dr = DataReader(1000)
+    channels = 5
+    sources = 2
+    writer = csvWriter(channels, sources)
+    writer.intro()
+    # dr = DataReader(1000)
     focusTime = 5 * 60
     finalTime = time.time() + focusTime
-    fileHeader = ",".join(dr.get_names()) + ", direction\n" 
+    fileHeader = ",".join(writer.dr.get_names()) + ", direction\n" 
     fileNameBin = dataGen.findEmptyFile("bin")
     fileNameRaw = dataGen.findEmptyFile("raw")
     file = open(fileNameRaw, "w")
     file.write(fileHeader)
     file.close()
-    intro()
+    
 
     while(time.time() < finalTime):
         
-            inputLoop(dr, fileNameBin, fileNameRaw)
+            writer.inputLoop()
 
                 
 
