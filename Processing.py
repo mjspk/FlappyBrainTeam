7a08b2ab7e1cfa34a02687dd2e2a622bf5ea5675
gcrookes <crookes.garnet@gmail.com>
2022-07-30 13:46:17 -0600

    Updated to work with multiple sensor inputs

--- Processing.py
+++ Processing.py
@@ -7,18 +7,17 @@ from queue import Queue
 class DataReader():
 
     def __init__(self, buffer_length):
-        self.ser = serial.Serial("COM4", 115200, timeout = .001)
-        time.sleep(1)
+        self.ser = serial.Serial("COM4", 115200, timeout = .00001)
         self.ser.flushInput()
         self.ser.flushOutput()
+
+        time.sleep(5)
         self.data = Queue(buffer_length)
         self.bin_names = ["Delta", "Theta", "Alpha", "Beta", "Gamma"]
         self.bands = [4, 8, 12, 30, 100]
 
     def get_data(self, plot = False):
-        
-        # data = np.array([[2,2],[2,2]])
-        
+    
         # Throw away top line
         self.ser.readline()
 
@@ -27,6 +26,11 @@ class DataReader():
 
         # Read in data until the end of the file
         while (len(input_decoded) > 3 and input_decoded[-3] == ">"):
+
+            if (input_decoded[0] != "<"):
+                input_raw = self.ser.readline()
+                input_decoded = input_raw.decode()
+                continue
 
             if len(input_raw) > 5:
                 input_string  = input_decoded.strip().replace("<","").replace(">","")
@@ -43,25 +47,46 @@ class DataReader():
                 input_raw = self.ser.readline()
                 input_decoded = input_raw.decode()
 
+            
+
         # Convert the data left in teh queue to a numpy array
         data_array = np.array(self.data.queue)
-
-        # Do a FFT on the data
-        fftData = np.fft.fft(data_array[:,1])
-        fftData = np.sqrt(fftData.real**2 + fftData.imag**2)
         freq = np.fft.fftfreq(data_array.shape[0]) * np.average(data_array[:,0])
-
-        fftData = fftData[0:len(fftData)//2]
         freq = freq[0:len(freq)//2]
 
-        bands = self.create_bands(freq, fftData)
+        fftData = None
+        bandsData = None
+
+        for i in range (1, data_array.shape[1]):
+
+            # Do a FFT on the data
+            fftReading = np.fft.fft(data_array[:,i])
+            fftReading = fftReading[0:len(fftReading)//2]
+            fftReading = np.sqrt(fftReading.real**2 + fftReading.imag**2)  
+
+            if fftData is None:
+                fftData = fftReading
+            else:
+                fftData = np.vstack((fftData, fftReading))
+                y, x = fftData.shape
+                fftData = fftData.reshape((x,y))
+
+                
+            bands = self.create_bands(freq, fftReading)
+
+            if bandsData is None:
+                bandsData = bands
+            else:
+                bandsData = np.vstack((bandsData, bands))
+                y, x = bandsData.shape
+                bandsData = bandsData.reshape((x,y))
 
         if plot:
-            plt.stem(freq, fftData, markerfmt=" ")
+            plt.stem(freq, fftReading, markerfmt=" ")
             plt.show(block = True)
 
         # Return results of the fourier transform
-        return freq, fftData, bands
+        return freq, fftData, bandsData
 
     def get_names(self):
         return self.bin_names
@@ -92,10 +117,7 @@ def plot_bands(bands, bin_names):
 if __name__ == "__main__":
 
     # Create Data reader with a queue length
-    dr = DataReader(1000)
-
-    # Give it a bit of time for the data to accumulate
-    time.sleep(3)
+    dr = DataReader(500)
 
     bin_names = ["Delta", "Theta", "Alpha", "Beta", "Gamma"]
     bin_range = [4, 8, 12, 30, 100]
@@ -108,19 +130,15 @@ if __name__ == "__main__":
     plt.show(block = False)
     ax = plt.gca()
     plt.ylabel("Amplitude")
-    # max_y = 0
+
 
     while True:
 
         frequencies, amplitudes, bands = dr.get_data()
 
         plt.cla()
-        bar = plt.bar(bin_names, bands, color="#7967e1")
+        bar = plt.bar(bin_names, bands[:,0], color="#7967e1")
 
-        # max_y = max(max_y, max(bands))
-        # ax.set_ylim([0,max_y])
-
-        print(f"Bands: {bands} + Total Length: {amplitudes.shape}")
         
         plt.pause(0.01)
         time.sleep(1)
